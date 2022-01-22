f309d80c5757e2b14b851a762cae9892ce5ad9eb
Buhari Nasir Maiwada <maewada@live.com>
2022-01-22 15:06:14 -0500

    Completed Header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,63 @@
-import React from "react"
-import Navbar from "./components/Navbar"
+import React from "react";
+import Navbar from "./components/Navbar";
+import Header from "./components/Header";
+
+//adding particles js @https://www.npmjs.com/package/tsparticles
+import Particles from "react-tsparticles";
 // import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <>
+      <Particles
+        className="tsparticles-canvas-el"
+        params={{
+          particles: {
+            color: {
+              value: "#f9ab00",
+            },
+            links: {
+              color: "#ffffff",
+              distance: 150,
+              enable: true,
+              opacity: 1,
+              width: 4,
+            },
+            collisions: {
+              enable: true,
+            },
+            move: {
+              direction: "none",
+              enable: true,
+              outMode: "bounce",
+              random: false,
+              speed: 6,
+              straight: false,
+            },
+            number: {
+              value: 30,
+              density: {
+                enable: true,
+                value_area: 900,
+              },
+              opacity: {
+                value: 0.1,
+              },
+              shape: {
+                type: "circle",
+                stroke: {
+                  width: 6,
+                  color: "#f9ab00",
+                },
+              },
+            },
+          },
+          detectRetina: true,
+        }}
+      />
       <Navbar />
+      <Header />
     </>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,35 @@
+import React from "react"
+//adding react typed
+import Typed from "react-typed"
+//Adding React Social icon (https://jaketrent.github.io/react-social-icons/)
+import { SocialIcon } from "react-social-icons";
+
+
+const Header = () => {
+    return (
+        
+        <div className="header-wrapper" id="home" style={{backgroundImage: "url(/images/Background/header-background.jpeg')"}} >
+            <div className="main-info">
+                <h1 class="font-effect-fire-animation"><span id="name">I'm Buhari Nasir Maiwada</span> </h1>
+                <hr />
+                <h1> Computer Engineer | Software Engineer </h1>
+                <Typed
+                    className="typed-text"
+                    strings={["Computer Engineering (Internet Of Things) "," Fullstack Web Developer", "Frontend Web Developer", "Backend Web Developer"]}
+                    typeSpeed={70}
+                    backSpeed={60}
+                    loop
+                />
+                <a href="#contacts" className="btn-main-offer btn-lg" > contact me</a>
+                <span>
+                    
+                    <SocialIcon url="https://linkedin.com/in/buharimaiwada" network="linkedin" style={{margin: 10, padding: 10, height: 75, width: 75}} target="_blank" />
+                    
+                    <SocialIcon url="https://github.com/BsoftMaewada" network="github" bgColor="grey" style={{margin: 10, padding: 10, height: 75, width: 75}} target="_blank"/>
+                </span>
+            </div>
+        </div>
+    )
+}
+
+export default Header
