56686c65ddb811a817699f046b0ceef6ea890962
Buhari Nasir Maiwada <maewada@live.com>
2022-01-22 19:01:32 -0500

    Education Completed

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import Header from "./components/Header";
 import AboutMe from "./components/AboutMe"
+import Education from "./components/Education"
 
 //adding particles js @https://www.npmjs.com/package/tsparticles
 import Particles from "react-tsparticles";
@@ -60,6 +61,7 @@ function App() {
       <Navbar />
       <Header />
       <AboutMe />
+      <Education />
     </>
   );
 }
---
+++ src/components/Education.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faBriefcase, faFile } from "@fortawesome/free-brands-svg-icons";
+import { faUserGraduate, faFileCode } from "@fortawesome/free-solid-svg-icons";
+
+const Education = () => {
+    return (
+        <div className="education" id="education">
+            <h1 className="py-5"> Education </h1>
+                <div className="container">
+                    <div className="row">
+                        <div className="col-lg-3 col-md-6 col-sm-6">
+                            <div className="box">
+                                <div className="circle"><FontAwesomeIcon className="icon" icon={faFileCode} size="2x" /></div>
+                                
+                                <h3> Software Engineering</h3>
+                                <h5>General Assembly</h5>
+                                <br /> <br />
+                                <p>Graduated:  2021<br />
+                                    Certificate in Software Engineering
+                                </p>
+                            </div>
+                        </div>
+                        {/* - */}
+                        <div className="col-lg-3 col-md-6 col-sm-6">
+                            <div className="box">
+                                <div className="circle"><FontAwesomeIcon className="icon" icon={faUserGraduate} size="2x" /></div>
+                                
+                                <h3> Computer Engineering</h3>
+                                <h5>Minnesota State University Mankato</h5>
+                                <br />
+                                <p>Graduated:  2020 <br />
+                                    Bachelor Degree in Computer Engineering</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    )
+}
+
+export default Education
