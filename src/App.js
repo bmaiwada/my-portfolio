a714eb758a01d2c324cf0b7e5c3f18485606b308
Buhari Nasir Maiwada <maewada@live.com>
2022-01-22 20:18:00 -0500

    Experience Completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
+
+//import components
 import Navbar from "./components/Navbar";
 import Header from "./components/Header";
 import AboutMe from "./components/AboutMe"
 import Skills from "./components/Skills"
 import Education from "./components/Education"
 // import Services from "./components/Services"
+import Experience from "./components/Experience"
 
 //adding particles js @https://www.npmjs.com/package/tsparticles
 import Particles from "react-tsparticles";
@@ -66,6 +69,7 @@ function App() {
       <Skills />
       <Education />
       {/* <Services /> */}
+      <Experience />
     </>
   );
 }
---
+++ src/components/Experience.js
@@ -0,0 +1,139 @@
+import React from "react";
+
+const Experience = () => {
+  return (
+    <div className="experience" id="experience">
+      <div className="d-flex justify-content-center my-5">
+        <h1> Experience </h1>
+      </div>
+      <div className="container experience-wrapper">
+        <div className="timeline-block timeline-block-right">
+          <div className="marker"></div>
+          <div className="timeline-content">
+            <h3>Software Engineer</h3>
+            <h5>General Assembly (Remote)</h5>
+            <h6> 2020 - 2021 </h6>
+            <ul>
+              <li>Fullstack, Frontend and Backend web developer.</li>
+              <li>Coding using languages such as: JavaScript, Python and C.</li>
+              <li>Knowledge on fundamentals of HTML, CSS, and JavaScript.</li>
+              <li>
+                Experience using frameworks such as NodeJS, Express JS, React JS
+                andDjango.
+              </li>
+              <li>
+                Knowledge on JavaScript ES6 features such as classes, arrow
+                functions, string templates, REST/spread operators, promises,
+                async/await.
+              </li>
+              <li>
+                Debugging, deploy, testing codes and seeking answers to
+                problems.
+              </li>
+              <li>
+                Using git and GitHub during the training with supervisors and
+                collaborate with coworkers on team projects.
+              </li>
+              <li>
+                Working with other technologies like GitHub, VSCode, Insomia,
+                Discord, Slack.
+              </li>
+              <li>Working with Database PostgreSQL, MongoDB and Heroku.</li>
+              <li>
+                Keeping track of changes made to code with Git version control
+                system.
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      {/* - */}
+      <div className="container experience-wrapper">
+        <div className="timeline-block timeline-block-left">
+          <div className="marker"></div>
+          <div className="timeline-content">
+            <h3>Pressure Transmitter Builder II</h3>
+            <h5>Emerson Automation Solution Inc</h5>
+            <h6> 2018 - 2020 </h6>
+            <ul>
+              <li>
+                Performed a wide variety of tasks at workstations, such as: hand
+                soldering; brazing; making repairs and performing quality check
+                testing using software application.
+              </li>
+              <li>
+                Performed calibration and testing in production lines as per
+                defined procedures.
+              </li>
+              <li>Wireless Pressure Gauge (WPG).</li>
+              <li>Wireless Temperature Transmitter.</li>
+              <li>Wireless Differential Pressure Flow Transmitter.</li>
+              <li>
+                Assembled devices used for measurement in a range of industries.
+              </li>
+              <li>Inspected work on a consistent basis to ensure quality.</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      {/* - */}
+      {/* <div className="container experience-wrapper">
+                <div className="timeline-block timeline-block-right">
+                    <div className="marker"></div>
+                    <div className="timeline-content">
+                        <h3> 2018 - 2019 </h3>
+                        <p>
+                            Lorem Ipsum is Lorem Ipsum, Lorem Ipsum is a utility and personal
+                            computer science library that holds Lorem Ipsum.
+                            Lorem Ipsum is also known as "the Lorem Ipsum"
+                        </p>
+                    </div>
+                </div>
+            </div> */}
+      {/* - */}
+      {/* <div className="container experience-wrapper">
+                <div className="timeline-block timeline-block-left">
+                    <div className="marker"></div>
+                    <div className="timeline-content">
+                        <h3> 2020 - 2021 </h3>
+                        <p>
+                            Lorem Ipsum is Lorem Ipsum, Lorem Ipsum is a utility and personal
+                            computer science library that holds Lorem Ipsum.
+                            Lorem Ipsum is also known as "the Lorem Ipsum"
+                        </p>
+                    </div>
+                </div>
+            </div> */}
+      {/* - */}
+      {/* <div className="container experience-wrapper">
+                <div className="timeline-block timeline-block-right">
+                    <div className="marker"></div>
+                    <div className="timeline-content">
+                        <h3> 2021 - 2022 </h3>
+                        <p>
+                            Lorem Ipsum is Lorem Ipsum, Lorem Ipsum is a utility and personal
+                            computer science library that holds Lorem Ipsum.
+                            Lorem Ipsum is also known as "the Lorem Ipsum"
+                        </p>
+                    </div>
+                </div>
+            </div>
+            - */}
+      {/* <div className="container experience-wrapper">
+                <div className="timeline-block timeline-block-left">
+                    <div className="marker"></div>
+                    <div className="timeline-content">
+                        <h3> 2022 - 2023 </h3>
+                        <p>
+                            Lorem Ipsum is Lorem Ipsum, Lorem Ipsum is a utility and personal
+                            computer science library that holds Lorem Ipsum.
+                            Lorem Ipsum is also known as "the Lorem Ipsum"
+                        </p>
+                    </div>
+                </div>
+            </div> */}
+    </div>
+  );
+};
+
+export default Experience;
