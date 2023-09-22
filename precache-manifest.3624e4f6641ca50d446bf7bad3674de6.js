b2dc01a4928d6d10274ea66b2c9db13b06f1759e
BsoftMaewada <maewada@live.com>
2023-09-22 10:06:08 -0400

    Updates

---
+++ precache-manifest.3624e4f6641ca50d446bf7bad3674de6.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = [
+  {
+    "revision": "2a4f5a6626432f036a62",
+    "url": "/my-portfolio/static/css/main.b937c002.chunk.css"
+  },
+  {
+    "revision": "2a4f5a6626432f036a62",
+    "url": "/my-portfolio/static/js/main.2a4f5a66.chunk.js"
+  },
+  {
+    "revision": "e365fac8f9a138c9a99b",
+    "url": "/my-portfolio/static/js/1.e365fac8.chunk.js"
+  },
+  {
+    "revision": "a0d736d171a9662a15c9",
+    "url": "/my-portfolio/static/css/2.daeef438.chunk.css"
+  },
+  {
+    "revision": "a0d736d171a9662a15c9",
+    "url": "/my-portfolio/static/js/2.a0d736d1.chunk.js"
+  },
+  {
+    "revision": "fab9387a48b5eb8e6f91",
+    "url": "/my-portfolio/static/js/runtime~main.fab9387a.js"
+  },
+  {
+    "revision": "a5ddee607d50349a40243ff846e829a9",
+    "url": "/my-portfolio/static/media/Portfolio.a5ddee60.png"
+  },
+  {
+    "revision": "1be7da4d9ad8a915cdc995b7e071594a",
+    "url": "/my-portfolio/static/media/HiT-Me.1be7da4d.png"
+  },
+  {
+    "revision": "8b2d6436a8dad26ee24f679c785ad3ea",
+    "url": "/my-portfolio/static/media/AssetsTracker.8b2d6436.jpg"
+  },
+  {
+    "revision": "9255e2dbb090602e7f2c166d77f1b1a2",
+    "url": "/my-portfolio/static/media/DevBook.9255e2db.png"
+  },
+  {
+    "revision": "00c453bd54ccd47cff49fba049e1d6e1",
+    "url": "/my-portfolio/static/media/MERN.00c453bd.png"
+  },
+  {
+    "revision": "e31eff851d46bb999baea0a0a1083e17",
+    "url": "/my-portfolio/static/media/FinchCollector.e31eff85.png"
+  },
+  {
+    "revision": "c6f47129050ccd3b3d0d7a83e835122c",
+    "url": "/my-portfolio/static/media/Wayfarer.c6f47129.png"
+  },
+  {
+    "revision": "c0a1083bbf95a5ab25d7385fc22d09b0",
+    "url": "/my-portfolio/static/media/CarDealership.c0a1083b.png"
+  },
+  {
+    "revision": "0ffb0e4f02c2dae356f00fdca3b79333",
+    "url": "/my-portfolio/static/media/portfolio-logo.0ffb0e4f.png"
+  },
+  {
+    "revision": "6f79f6cf841de556f36468e7b9894dc0",
+    "url": "/my-portfolio/static/media/Software Engineer .6f79f6cf.pdf"
+  },
+  {
+    "revision": "543b0cc6b2f456a4c67108f223fa5071",
+    "url": "/my-portfolio/static/media/myself.543b0cc6.png"
+  },
+  {
+    "revision": "4a2e28c0b1785492028619d33d5a52b5",
+    "url": "/my-portfolio/static/media/assembly-language-removebg-preview.4a2e28c0.png"
+  },
+  {
+    "revision": "dddfc3565e43456d21cf4d6637019e9b",
+    "url": "/my-portfolio/static/media/Keil.dddfc356.svg"
+  },
+  {
+    "revision": "fba8db5e90a21d98edea6c695292c4d3",
+    "url": "/my-portfolio/static/media/Arm_Cortex-removebg-preview.fba8db5e.png"
+  },
+  {
+    "revision": "fd435cdd6d8691658996e3caeabc1dcc",
+    "url": "/my-portfolio/static/media/Computer_Network-removebg-preview.fd435cdd.png"
+  },
+  {
+    "revision": "6e4f43a809e01bb964bf255d5f20f208",
+    "url": "/my-portfolio/static/media/IoT-removebg-preview.6e4f43a8.png"
+  },
+  {
+    "revision": "7f2983d3efbd8a8519a9837fcbdc3af6",
+    "url": "/my-portfolio/static/media/Computer_Hardware-removebg-preview.7f2983d3.png"
+  },
+  {
+    "revision": "eb7ecde26f33b85d920fd077703d52f2",
+    "url": "/my-portfolio/static/media/stm32cubemx.eb7ecde2.png"
+  },
+  {
+    "revision": "0ed49968dfd715f74e3ae94054c450a6",
+    "url": "/my-portfolio/static/media/rtos-logo-removebg-preview.0ed49968.png"
+  },
+  {
+    "revision": "0588e9c4278254288181d3a6faaa3e66",
+    "url": "/my-portfolio/static/media/STM32-removebg-preview.0588e9c4.png"
+  },
+  {
+    "revision": "59f8fd04c4cff6214a69d205f66e1859",
+    "url": "/my-portfolio/static/media/Tracking-Vehicle.59f8fd04.MP4"
+  },
+  {
+    "revision": "98c286d76206616afc701d58a338dc90",
+    "url": "/my-portfolio/static/media/Bluetooth-Sensor.98c286d7.MP4"
+  },
+  {
+    "revision": "3301ef969d358cd366640bfcc18c2c7b",
+    "url": "/my-portfolio/static/media/header-background.3301ef96.jpeg"
+  },
+  {
+    "revision": "a6e9475ca37e851922761c7f3118079f",
+    "url": "/my-portfolio/static/media/testimonials-bg.a6e9475c.jpg"
+  },
+  {
+    "revision": "f434311f5b8fce5415b50445fefe98d5",
+    "url": "/my-portfolio/index.html"
+  }
+];
